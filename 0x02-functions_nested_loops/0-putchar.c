bf700cebfe3ff6b5fe6dcf4a979e46b9ee7ad81b
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-03-21 04:26:16 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,7 +8,7 @@
 int main()
 {
 	int i, j;
-	for (i =1; i<=5; i++)
+	for (i =1; i<=1; i++)
 	{
 		for (j =1;j <=i; j++)
 		{
