e4bb38bb848dd4033de63fa6e7b2c65d57f58233
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-04-03 04:03:57 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * _memcpy - function that copies memory
  * @n: number of bytes to copy
  * @src: memory arear to copy from
- * dest: array fo copy into
+ * @dest: array fo copy into
  * Return: Pointer to dest
  */
 
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *me;
 
 	me = dest;
-	while (n >0)
+	while (n > 0)
 	{
 		*dest = *src;
 		dest++;
