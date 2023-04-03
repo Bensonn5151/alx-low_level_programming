2519ac74dacddd480b96ee3c93d5b71659d83d0e
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-04-03 06:42:25 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,17 +10,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j, k;
-	for (i = 0; haystack[i] != '\0'; i++)
+	int i, j, k;
+
+	i = 0;
+	k = 0;
+	while (haystack[i] != '\0')
 	{
-		if (haystack[i] == needle[0])
+		j = 0;
+		while (needle[j + k] != '\0' && haystack[i + k] != '\0' && needle[j + k] == haystack[i + k])
 		{
-			for (j = i, k = 0; needle[k] != '\0' && haystack[j] == needle [k]; j++, k++);
-			if (needle[k] == '\0')
-			{
-				return (&haystack[i]);
-			}
+			if (haystack[i + k] != needle[j + k])
+				break;
+			k++;
 		}
+		if (needle[j + k] == '\0')
+			return (&haystack[i]);
+		j++;
+		i++;
 	}
 	return (NULL);
 }
