31b4238cd7d92f895405c690c08b8e693a4b0f73
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-04-05 01:47:29 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  function that prints a string, followed by a new line
+ * @s: An input string to printing
+ * Return: Nothing
+ */
+void _puts_recursion(char *st)
+{
+	if (*st)
+	{
+		_putchar(*st);
+		_puts_recursion(st + 1);
+	}
+	else
+		_putchar('\n');
+}
