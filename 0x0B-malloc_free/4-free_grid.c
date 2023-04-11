e61c179890e27d217f88f6bbc479fab14b0b0050
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-04-11 04:45:11 -0700

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D array of ints
+ *
+ * @grid: input 2D array of int to free
+ * @height number of rows in grid
+ *
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x = 0;
+
+	while (x < height)
+	{
+		free(grid[x]);
+		x++;
+	}
+	free(grid);
+}
