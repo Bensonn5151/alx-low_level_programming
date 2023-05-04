5c57c2f6a26f7cabd1e9e4500a7dfc8ab9b6214c
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-05-04 04:27:26 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * print_binary - A function that prints a binary number without % or /
- * @n: The number to be printed
- * Return: Nothing
+ * print_binary - prints the binary representation of a number
+ * @n: the number to convert
+ * Return: nothing
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned int min_n = 0, max_n = 32768;
+	int b = 0;
+	int bit = sizeof(n) * 8 - 1;
 
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-	while (max_n)
+
+	while (bit >= 0)
 	{
-		if (min_n == 1 && (n & max_n) == 0)
-			_putchar('0');
-		else if ((n & max_n) != 0)
+		if (n >> bit & 1)
 		{
 			_putchar('1');
-			min_n = 1;
+			b++;
 		}
-		max_n >>= 1;
+		else if (b)
+			_putchar('0');
+		bit--;
 	}
 }
